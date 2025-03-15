618f5f931f5b512e4d51d4dc36d6b3ef9488a0cb
J.Dondlinger <johndondlinger21@gmail.com>
2025-03-15 14:17:03 -0500

    update overhaul functionality

---
+++ src/service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'stosh-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/components/stosh.html',
+    '/styles/critical.css',
+    '/assets/banner.webp',
+    '/assets/icon-192x192.png',
+    '/assets/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request)
+                    .then(response => {
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+                        const responseToCache = response.clone();
+                        caches.open(CACHE_NAME)
+                            .then(cache => {
+                                cache.put(event.request, responseToCache);
+                            });
+                        return response;
+                    });
+            })
+    );
+});
